6cf8a031e08f57b938f675750006f622d99c7145
sorydi3 <ibrahimasory22@hotmail.com>
2021-04-06 18:06:46 +0100

    added input exemples

--- practhask.hs
+++ practhask.hs
@@ -27,12 +27,13 @@ freeVars (AP a b) = freeVars a `union` freeVars b
 --($) :: (a -> b) -> a -> b 
 freeVars (La v lt) = delete v $ freeVars lt
 
-
+-- freeAndBoundVars (La "y" (AP (Va "y") (La "x" (AP (Va "x") (Va "z")))))
 freeAndBoundVars :: LT -> ([Var],[Var])
 freeAndBoundVars (Va v) = ([v],[])
 freeAndBoundVars (AP a b) = (fst (freeAndBoundVars a) `union`  fst (freeAndBoundVars b),snd (freeAndBoundVars a) `union` snd (freeAndBoundVars b))
 freeAndBoundVars (La v lt) = (delete v $ fst (freeAndBoundVars lt),if v `elem` fst (freeAndBoundVars lt) then v:snd (freeAndBoundVars lt) else snd (freeAndBoundVars lt))
 
+-- subs_i (La "x" (AP (Va "x") (Va "x"))) "x" (AP (Va "y") (Va "z"))
 subs_i :: LT->Var->LT->LT
 subs_i var@(Va v) x e = if v==x then e else var
 subs_i ap@(AP a b) x e = AP (subs_i a x e) (subs_i b x e)   
@@ -40,6 +41,8 @@ subs_i la@(La v lt) x e | x == v = subs_i lt x e
                         | otherwise = La v (subs_i lt x e)
 
 
+--  subs (La "x" (AP (Va "x") (La "x" (Va "x")))) (AP (Va "y") (Va "z"))
+--WORKING ON IT
 subs::LT->LT->LT
 subs var@(Va x) _ = error "No es un redex"
 subs (AP a b) e =  AP (subs a b) e 
@@ -47,7 +50,14 @@ subs exp@(La x lt) e = subs_i exp x e
 
 
 
+
+
 -- 3 -- 
+
+esta_normal::LT->Bool
+esta_normal (Va v) = True
+--esta_normal (La v lt) | lt == LT
+esta_normal (AP a b) = esta_normal a
 
 -- 3 
 
